7ac8b1e4385434239e705ea8fac327290374d1c6
tblue160397 <64902763+tblue160397@users.noreply.github.com>
2021-04-28 20:24:37 +0700

    add bottm navigation bar

--- MyApplication/app/src/main/java/com/example/myapplication/Auth/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Auth/LoginActivity.java
@@ -9,26 +9,42 @@ import android.widget.Button;
 
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LoginActivity extends AppCompatActivity {
     private Button backToStartBtn;
+    private Button doLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         backToStartBtn = findViewById(R.id.lBackToStartBtn);
+        doLoginBtn = findViewById(R.id.doLoginBtn);
 
+        /* back to Start Screen */
         backToStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 backToStart();
             }
         });
+
+        /* go to Home */
+        doLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToHome();
+            }
+        });
     }
 
     public void backToStart(){
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void goToHome(){
+        Intent intent = new Intent(this, BottomNavigationView.class);
         startActivity(intent);
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ContentApp/BottomNavbarActivity.java
@@ -0,0 +1,59 @@
+package com.example.myapplication.ContentApp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.myapplication.AddEventFragment;
+import com.example.myapplication.CalendarFragment;
+import com.example.myapplication.HomeFragment;
+import com.example.myapplication.ProfileFragment;
+import com.example.myapplication.R;
+import com.example.myapplication.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavbarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navbar);
+
+        BottomNavigationView toolbar = findViewById(R.id.bottomnavigation);
+        toolbar.setOnNavigationItemSelectedListener(navlistener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.navigation_add_event:
+                            selectedFragment = new AddEventFragment();
+                            break;
+                        case R.id.navigation_calendar:
+                            selectedFragment = new CalendarFragment();
+                            break;
+                        case R.id.navigation_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment,selectedFragment).commit();
+                    return true;
+                }
+            };
+}
