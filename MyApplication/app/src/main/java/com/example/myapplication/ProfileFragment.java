7a18fa85f3276407f95d60c35d6f8a8565f3dfe7
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-05-03 22:30:52 +0700

    Add Login_Register by email_password - ThinhMD

--- MyApplication/app/src/main/java/com/example/myapplication/ProfileFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
     private Button logoutBtn;
 
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
